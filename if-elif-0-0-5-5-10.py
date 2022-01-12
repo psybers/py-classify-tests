add4cf9c351d87cc801f848d498278ffe4bdd6b2
Robert Dyer <rdyer@unl.edu>
2022-01-12 16:38:18 -0600

    update test

--- if-elif-0-0-5-5-10.py
+++ if-elif-0-0-5-5-10.py
@@ -1,6 +1,10 @@
 temprature = 35
 if temprature > 30:
-	print( " It is a hot day")
+	print( "It is a hot day")
 elif temprature > 20:
 	print ("It is a pleasant day")
+elif temprature > 10:
+	print ("It is cool")
+else:
+	print ("Brr")
 print("done")
